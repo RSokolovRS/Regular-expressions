4213096b0a78fbb5e98bcb7a2f94c3f1508bceb1
RSokolov <roman-27@inbox.ru>
2023-04-12 22:14:10 +0300

    HW_add

--- data/read.py
+++ data/read.py
@@ -3,6 +3,23 @@ import csv
 
 
 with open("data/phonebook_raw.csv", encoding='utf-8') as f:
-  rows = csv.reader(f, delimiter=",", )
+  rows = csv.reader(f, delimiter=",")
   contacts_list = list(rows)
-pprint(contacts_list)
+  a = contacts_list[0]
+  # v = str([i for i in a])
+  # print(v)
+
+with open("data/phonebook_raw.csv", encoding='utf-8') as f:
+  rows = csv.DictReader(f, delimiter=",")
+  contacts_list = list(rows)
+  for i in contacts_list:
+    # print(i)
+    for v in i.items():
+      print(v)
+
+  # b = sorted(rows, key=lambda x: x['lastname', 'firstname'])
+  # pprint(b)
+# print(a[0:])
+# for i in a:
+
+#   print(i)
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from data.read import contacts_list
 
 
-,contacts_list
-print(contacts_list)
+contacts_list
