0b35da5174d1790800330ce6b6bff77f9b929691
RSokolov <roman-27@inbox.ru>
2023-04-17 06:29:44 +0300

    New

--- data/read.py
+++ data/read.py
@@ -5,20 +5,22 @@ import csv
 with open("data/phonebook_raw.csv", encoding='utf-8') as f:
   rows = csv.reader(f, delimiter=",")
   contacts_list = list(rows)
-  a = contacts_list[0]
-  # v = str([i for i in a])
-  # print(v)
+# a = contacts_list
+# v = a[0]
+# pprint(v)
+#   # v = str([i for i in a])
 
-with open("data/phonebook_raw.csv", encoding='utf-8') as f:
-  rows = csv.DictReader(f, delimiter=",")
-  contacts_list = list(rows)
-  for i in contacts_list:
-    # print(i)
-    for v in i.items():
-      print(v)
+# with open("data/phonebook_raw.csv", encoding='utf-8') as f:
+#   rows = csv.DictReader(f, delimiter=",")
+#   contacts_list = list(rows)
+#   print(contacts_list[0])
+# #   for i in contacts_list:
+# #     # print(i)
+# #     for v in i.items():
+# #       print(v)
 
-  # b = sorted(rows, key=lambda x: x['lastname', 'firstname'])
-  # pprint(b)
+#   b = sorted(rows, key=lambda x: x['firstname'])
+#   pprint(b)
 # print(a[0:])
 # for i in a:
 
--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 from data.read import contacts_list
 
 
-contacts_list
+print(contacts_list[1:])
+
+
+
+
+
+# def check(lastname, firstname, surname, organization, position, phone, email):
+
+# print(contacts_list)
+# for i in contacts_list:
+#     print(i)
+
+
