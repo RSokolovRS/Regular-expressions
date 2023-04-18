9f48fbe0d9caef8999112a087da297091269e39a
RSokolov <roman-27@inbox.ru>
2023-04-18 15:49:50 +0300

    HW_code

--- main.py
+++ main.py
@@ -1,16 +1,67 @@
-from data.read import contacts_list
+import csv
+import re
+
+def read(file):
+   with open(file, encoding='utf-8') as f:
+    rows = csv.reader(f, delimiter=",")
+    contacts_list = list(rows)
+    return  contacts_list
+
+def format_number(contacts_list):
+    pattern = r'(\+7|8)(\s*)(\(*)(\d{3})(\)*)(\s*)' \
+                            r'(\-*)(\d{3})(\s*)(\-*)(\d{2})(\s*)(\-*)' \
+                            r'(\d{2})(\s*)(\(*)(доб)*(\.*)(\s*)(\d+)*(\)*)'
+    pattern_bloc = r'+7(\4)\8-\11-\14\15\17\18\19\20'
+    contacts_list_new = list()
+    for value in contacts_list:
+        values = ','.join(value)
+        new_values = re.sub(pattern, pattern_bloc, values)
+        list_new = new_values.split(',')
+        contacts_list_new.append(list_new)
+    return contacts_list_new
+
+def format_full_name(contacts_list):
+    pattern = r'^([А-ЯЁа-яё]+)(\s*)(\,?)([А-ЯЁа-яё]+)' \
+                       r'(\s*)(\,?)([А-ЯЁа-яё]*)(\,?)(\,?)(\,?)'
+    pattern_bloc = r'\1\3\10\4\6\9\7\8'
+    contacts_list_new = list()
+    for value in contacts_list:
+        values = ','.join(value)
+        new_values = re.sub(pattern, pattern_bloc, values)
+        list_new = new_values.split(',')
+        contacts_list_new.append(list_new)
+    return contacts_list_new
+
+def join_duplicates(contacts_list):
+    for i in contacts_list:
+        for j in contacts_list:
+            if i[0] == j[0] and i[1] == j[1] and i != j:
+                if i[2] == (' '):
+                    i[2] = j[2]
+                if i[3] == (' '):
+                    i[3] = j[3]
+                if i[4] == (' '):
+                    i[4] = j[4]
+                if i[5] == (' '):
+                    i[5] = j[5]
+                if i[6] == (' '):
+                    i[6] = j[6]
+    contacts_list_new = list()
+    for value in contacts_list:
+        if value not in contacts_list_new:
+            contacts_list_new.append(value)
+    return contacts_list_new
+
+def write_file(contacts_list):
+    with open("data/phonebook.csv", "w", encoding='utf-8') as f:
+        data_writer = csv.writer(f, delimiter=',')
+        data_writer.writerows(contacts_list)
 
 
-print(contacts_list[1:])
-
-
-
-
-
-# def check(lastname, firstname, surname, organization, position, phone, email):
-
-# print(contacts_list)
-# for i in contacts_list:
-#     print(i)
-
-
+if __name__ == '__main__':
+    file = read("data/phonebook_raw.csv")
+    file = format_number(file)
+    file = format_full_name(file)
+    file = join_duplicates(file)
+    write_file(file)
+    print(file)
