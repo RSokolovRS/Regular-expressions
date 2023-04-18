1c79021cded47e9a78868862ad07bcbb3415803e
RSokolov <roman-27@inbox.ru>
2023-04-18 19:44:38 +0300

    New

--- main.py
+++ main.py
@@ -1,16 +1,2 @@
-from data.read import contacts_list
-
-
-print(contacts_list[1:])
-
-
-
-
-
-# def check(lastname, firstname, surname, organization, position, phone, email):
-
-# print(contacts_list)
-# for i in contacts_list:
-#     print(i)
 
 
